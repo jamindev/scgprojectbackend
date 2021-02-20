92c0025cd7989a54407ddfb815e3ba9fac3d65db
Benjamin Amoah <benjaminamoah@Benjamins-MBP.home>
2021-02-20 14:44:00 -0500

    8th commit

--- index.php
+++ index.php
@@ -4,16 +4,16 @@ if( $http_origin == "http://scgprojectfrontend.s3-website-us-east-1.amazonaws.co
     header("Access-Control-Allow-Origin: $http_origin");
 }
 
-require("./includes/manage_db.php");
+// require("./includes/manage_db.php");
 
 if(isset($_GET['msg'])){
     $email = "not yet";
-    $manage_db = new manage_db();
-    $query = $manage_db->return_query("SELECT * FROM users");
-    $i = 0;
-    while($i<mysqli_num_rows($query)){
-        $email = mysql_result($query, $i, 'email') or die(mysql_error());
-    }
+    // $manage_db = new manage_db();
+    // $query = $manage_db->return_query("SELECT * FROM users");
+    // $i = 0;
+    // while($i<mysqli_num_rows($query)){
+    //     $email = mysql_result($query, $i, 'email') or die(mysql_error());
+    // }
 
     $data1 = ["message1" => $email];
     echo json_encode($data1);
