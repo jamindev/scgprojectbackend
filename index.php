39e04323d2b6bae19aba2f1d6fe4d97ec88c3893
Benjamin Amoah <benjaminamoah@Benjamins-MBP.home>
2021-02-20 13:28:16 -0500

    8th commit

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-header("Access-Control-Allow-Origin: http://scgprojectfrontend.s3-website-us-east-1.amazonaws.com");
+$http_origin = $_SERVER['HTTP_ORIGIN'];
+if( $http_origin == "http://scgprojectfrontend.s3-website-us-east-1.amazonaws.com" || $http_origin == "http://24.190.177.13"){
+    header("Access-Control-Allow-Origin: $http_origin");
+}
 
 if(isset($_GET['msg'])){
     $data1 = ["message1" => "Actually getting data"];
