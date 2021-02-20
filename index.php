de569e873025e821076013ac41fccc52ec779ab2
Benjamin Amoah <benjaminamoah@Benjamins-MBP.home>
2021-02-20 11:42:51 -0500

    8th commit

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 $data = ["message" => "Serving up the SCGProject api..."];
 echo json_encode($data);
+$data1 = ["message1" => "Serving up the SCGProject api..."];
 
+if(isset($_GET['apple'])){
+    echo json_encode($data1);
+}
 ?>
