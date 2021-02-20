bb9a07ccc258c24165e1ff42c4eb8c274be63221
Benjamin Amoah <benjaminamoah@Benjamins-MBP.home>
2021-02-20 12:27:44 -0500

    8th commit

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://scgprojectfrontend.s3-website-us-east-1.amazonaws.com/*");
+
 if(isset($_GET['msg'])){
     $data1 = ["message1" => "Actually getting data"];
     echo json_encode($data1);
