31ce056239cbec32fc261d7d548bd4133978fb23
Benjamin Amoah <benjaminamoah@Benjamins-MBP.home>
2021-02-20 13:31:07 -0500

    8th commit

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $http_origin = $_SERVER['HTTP_ORIGIN'];
-if( $http_origin == "http://scgprojectfrontend.s3-website-us-east-1.amazonaws.com" || $http_origin == "http://24.190.177.13"){
+if( $http_origin == "http://scgprojectfrontend.s3-website-us-east-1.amazonaws.com" || $http_origin == "http://24.190.177.13" || $http_origin == "http://localhost:3000"){
     header("Access-Control-Allow-Origin: $http_origin");
 }
 
