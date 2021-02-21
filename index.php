ca29273981535f61842835f2ed19b9d925f14b76
Benjamin Amoah <benjaminamoah@Benjamins-MBP.home>
2021-02-20 19:37:16 -0500

    8th commit

--- index.php
+++ index.php
@@ -10,10 +10,6 @@ if(isset($_GET['msg'])){
     $email = "not yet";
     $manage_db = new manage_db();
     $query = $manage_db->return_query("SELECT * FROM users");
-    // $i = 0;
-    // while($i<mysqli_num_rows($query)){
-    //     $email = mysql_result($query, $i, 'email') or die(mysql_error());
-    // }
 
     if ($query->num_rows > 0) {
         // output data of each row
@@ -25,4 +21,34 @@ if(isset($_GET['msg'])){
     $data1 = ["message1" => $email];
     echo json_encode($data1);
 }
+
+
+if(isset($_POST['email'])){
+    $email = "not yet";
+    $manage_db = new manage_db();
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $gender = $_POST['gender'];
+    $date_of_birth = $_POST['date_of_birth'];
+    $address_1 = $_POST['address_1'];
+    $address_2 = $_POST['address_2'];
+    $city = $_POST['city'];
+    $country = $_POST['country'];
+    $state_or_region = $_POST['state_or_region'];
+
+    $query = $manage_db->return_query("INSERT INTO users VALUES(null, '$first_name', '$last_name', '$email', '$password', '$address_1', '$address_2', '$city', '$state_or_region', '$country', 'customer', 'active', CURRENT_TIMESTAMP, null)");
+
+    if ($query->num_rows > 0) {
+        // output data of each row
+        while($row = $query->fetch_assoc()) {
+            $email = $row['email'];
+        }
+    }
+
+    $data1 = ["response" => "posted", "email" => $email, "id" => "1"];
+    echo json_encode($data1);
+}
+
 ?>
