018c292bde0f9c9932b6523c00c57c97886e1f17
Benjamin Amoah <benjaminamoah@Benjamins-MBP.home>
2021-02-22 11:48:33 -0500

    8th commit

--- dashboard.php
+++ dashboard.php
@@ -3,27 +3,23 @@ header('Access-Control-Allow-Origin: *');
 
 require("./includes/manage_db.php");
 
-$data = ["response" => "some data"];
+if(isset($_GET['myprofile'])){
+    $email = $_GET['email'];
 
-echo "hello...";
+    $query = $manage_db->return_query("SELECT * FROM users WHERE email='$email'");
 
-// if(isset($_GET['myprofile'])){
-//     $email = $_GET['email'];
+    if ($query->num_rows > 0) {
+        while($row = $query->fetch_assoc()) {
+            $first_name = $row['first_name'];
+            $city = $row['city'];
+        }
+        $data = ["response" => "retrieved", "email" => $email, "first_name" => $first_name, "city"=> $city];
+    }else{
+        $data = ["response" => "failed"];
+    }
+    $data = ["response" => $query->num_rows];
 
-//     $query = $manage_db->return_query("SELECT * FROM users WHERE email='$email'");
-
-//     if ($query->num_rows > 0) {
-//         while($row = $query->fetch_assoc()) {
-//             $first_name = $row['first_name'];
-//             $city = $row['city'];
-//         }
-//         $data = ["response" => "retrieved", "email" => $email, "first_name" => $first_name, "city"=> $city];
-//     }else{
-//         $data = ["response" => "failed"];
-//     }
-//     $data = ["response" => $query->num_rows];
-
-//     echo json_encode($data);
-// }
+    echo json_encode($data);
+}
 
 ?>
--- signin.php
+++ signin.php
@@ -6,25 +6,16 @@ require("./includes/manage_db.php");
 
 if(isset($_GET['signin'])){
     $manage_db = new manage_db();
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
     $email = $_POST['email'];
     $password = md5($_POST['password']);
     $gender = $_POST['gender'];
-    $date_of_birth = $_POST['date_of_birth'];
-    $address_1 = $_POST['address_1'];
-    $address_2 = $_POST['address_2'];
-    $city = $_POST['city'];
-    $country = $_POST['country'];
-    $state_or_region = $_POST['state_or_region'];
 
-    $query = $manage_db->return_query("SELECT * FROM users WHERE email='$email'");
+    $query = $manage_db->return_query("SELECT * FROM users WHERE email='$email' AND password='$password'");
 
     if ($query->num_rows > 0) {
-        $data = ["response" => "Email is already being used!", "email" => $email, "id" => "1"];
+        $data = ["response" => "success", "email" => $email, "id" => "1"];
     }else{
-        $manage_db->query("INSERT INTO users VALUES(null, '$first_name', '$last_name', '$email', '$password', '$address_1', '$address_2', '$city', '$state_or_region', '$country', 'customer', 'active', CURRENT_TIMESTAMP, null)");
-        $data = ["response" => "posted", "email" => $email, "id" => "1"];
+        $data = ["response" => "failed", "email" => $email, "id" => "1"];
     }
 
     echo json_encode($data);
