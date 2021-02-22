7dd632ed8c17f9a6ffac541798040bf11ad68281
Benjamin Amoah <benjaminamoah@Benjamins-MBP.home>
2021-02-22 02:54:36 -0500

    8th commit

--- index.php
+++ index.php
@@ -30,12 +30,12 @@ require("./includes/manage_db.php");
 // }
 
 
-if(isset($_POST['email'])){
+if(isset($_POST['signup'])){
     $manage_db = new manage_db();
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
     $email = $_POST['email'];
-    $password = $_POST['password'];
+    $password = md5($_POST['password']);
     $gender = $_POST['gender'];
     $date_of_birth = $_POST['date_of_birth'];
     $address_1 = $_POST['address_1'];
@@ -44,16 +44,15 @@ if(isset($_POST['email'])){
     $country = $_POST['country'];
     $state_or_region = $_POST['state_or_region'];
 
-    $manage_db->query("INSERT INTO users VALUES(null, '$first_name', '$last_name', '$email', '$password', '$address_1', '$address_2', '$city', '$state_or_region', '$country', 'customer', 'active', CURRENT_TIMESTAMP, null)");
+    $query = $manage_db->return_query("SELECT * FROM users");
 
-    // if ($query->num_rows > 0) {
-    //     // output data of each row
-    //     while($row = $query->fetch_assoc()) {
-    //         $email = $row['email'];
-    //     }
-    // }
+    if ($query->num_rows > 0) {
+        $data = ["response" => "Email is already being used!", "email" => $email];
+    }else{
+        $manage_db->query("INSERT INTO users VALUES(null, '$first_name', '$last_name', '$email', '$password', '$address_1', '$address_2', '$city', '$state_or_region', '$country', 'customer', 'active', CURRENT_TIMESTAMP, null)");
+        $data = ["response" => "posted", "email" => $email, "id" => "1"];
+    }
 
-    $data = ["response" => "posted", "email" => $email, "password" => md5($password)];
     echo json_encode($data);
 }
 
