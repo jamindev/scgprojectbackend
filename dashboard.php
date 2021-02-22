f2ed175e2677be8f2b4efe163a57789f460ff97e
Benjamin Amoah <benjaminamoah@Benjamins-MBP.home>
2021-02-22 10:56:16 -0500

    8th commit

---
+++ dashboard.php
@@ -0,0 +1,25 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+
+require("./includes/manage_db.php");
+
+if(isset($_GET['myprofile'])){
+    $email = $_GET['email'];
+
+    $query = $manage_db->return_query("SELECT * FROM users WHERE email='$email'");
+
+    if ($query->num_rows > 0) {
+        while($row = $query->fetch_assoc()) {
+            $first_name = $row['first_name'];
+            $city = $row['city'];
+        }
+        $data = ["response" => "retrieved", "email" => $email, "first_name" => $first_name, "city"=> $city];
+    }else{
+        $data = ["response" => "failed"];
+    }
+    $data = ["response" => $query->num_rows];
+
+    echo json_encode($data);
+}
+
+?>
