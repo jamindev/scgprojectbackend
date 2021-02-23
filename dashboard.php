f085b2fadeba4b009d77f6aecc7886c554f5b52b
Benjamin Amoah <benjaminamoah@Benjamins-MBP.home>
2021-02-23 03:07:59 -0500

    8th commit

--- dashboard.php
+++ dashboard.php
@@ -43,7 +43,7 @@ if(isset($_POST['place_order'])){
     $data1 = $manufacturer+" "+$years+" "+$condition_description;
 
     //$manage_db->query("INSERT INTO orders VALUES(null, '$customer_id', '$payment_information_id', '$manufacturer', '$years', '$condition_decription', CURRENT_TIMESTAMP, null)");
-    $data = ["response" => "order_placed", "data" => $manufacturer];
+    $data = ["response" => "order_placed", "data" => $years];
     echo json_encode($data);
 }
 
