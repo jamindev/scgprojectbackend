ab85ce3a40c849142006b2175495cafca3965599
Benjamin Amoah <benjaminamoah@Benjamins-MBP.home>
2021-02-23 02:21:25 -0500

    8th commit

--- dashboard.php
+++ dashboard.php
@@ -24,24 +24,24 @@ if(isset($_GET['myprofile'])){
 
 
 if(isset($_POST['place_order'])){
-    // $manage_db = new manage_db();
-    // $customer_id = $_POST['id'];
-    // $manufacturer = $_POST['manufacturer'];
-    // $years = $_POST['years'];
-    // $condition_description = $_POST['condition_description'];
+    $manage_db = new manage_db();
+    $customer_id = $_POST['id'];
+    $manufacturer = $_POST['manufacturer'];
+    $years = $_POST['years'];
+    $condition_description = $_POST['condition_description'];
     
-    // // $query = $manage_db->return_query("SELECT * FROM payment_information WHERE customer_id='$id'");
+    // $query = $manage_db->return_query("SELECT * FROM payment_information WHERE customer_id='$id'");
 
-    // // if ($query->num_rows > 0) {
-    // //     while($row = $query->fetch_assoc()) {
-    // //         $payment_information_id = $row['id'];
-    // //     }
-    // // }
+    // if ($query->num_rows > 0) {
+    //     while($row = $query->fetch_assoc()) {
+    //         $payment_information_id = $row['id'];
+    //     }
+    // }
 
-    // // test payment_information_id
-    // $payment_information_id = 10;
+    // test payment_information_id
+    $payment_information_id = 10;
 
-    // $manage_db->query("INSERT INTO orders VALUES(null, $customer_id, $payment_information_id, '$manufacturer', '$years', '$condition_decription', CURRENT_TIMESTAMP, null)");
+    $manage_db->query("INSERT INTO orders VALUES(null, $customer_id, $payment_information_id, '$manufacturer', '$years', '$condition_decription', CURRENT_TIMESTAMP, null)");
     $data = ["response" => "order_placed"];
 
     echo json_encode($data);
